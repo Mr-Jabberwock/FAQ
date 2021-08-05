ea0ea9983c422ac953e74bc31c239b116b983f3d
Mr-Jabberwock <djohnsen143@gmail.com>
2021-08-05 13:10:22 +0200

    routing added

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>FAQ</div>
+    <div>
+       <NavigationBar/>
+    </div>
 </template>
+
 <script>
+import NavigationBar from '../components/NavigationBar'
+
+export default {
+  name: 'FAQ',
+  components: {
+    NavigationBar
+  }
+}
 
 </script>
 <style>
+
 
 </style>
