30088d58d5447b766562e9dfaade2af4cc715323
Mr-Jabberwock <djohnsen143@gmail.com>
2021-08-05 15:31:21 +0200

    added side menu and search bar

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,21 +1,30 @@
 <template>
     <div>
+       <SearchBar class="SearchBar"/>
        <NavigationBar/>
     </div>
 </template>
 
 <script>
 import NavigationBar from '../components/NavigationBar'
+import SearchBar from '../components/SearchBar'
 
 export default {
   name: 'FAQ',
   components: {
-    NavigationBar
+    NavigationBar,
+    SearchBar
   }
 }
 
 </script>
 <style>
+
+.SearchBar{
+    margin-bottom: 50px;
+    margin-left: 25%;
+    margin-top: 100px;
+}
 
 
 </style>
