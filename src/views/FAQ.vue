ca9bd2bb0bb11d36e4db8ca18fdd057fabc3fd58
Ibsylong <luanbanguyen94@gmail.com>
2021-08-05 16:04:01 +0200

    Fixes
    Fixes

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,22 +1,61 @@
 <template>
-  <div id="faq">
-    <navigation-bar></navigation-bar>
-    <accordion>
-        <topics>
+    <div id="faq">
+        <navigation-bar></navigation-bar>
+        <div class="faq__content">
+            <search-bar></search-bar>
+        <accordion>
+            <topics>
+            <!-- This slot will handle the title/header of the accordion and is the part you click on -->
+                <template slot="accordion-trigger">
+                    <h3>Item 1</h3>
+                </template>
+                <!-- This slot will handle all the content that is passed to the accordion -->
+                <template slot="accordion-content">
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+                </template>
+            </topics>
+
+            <topics>
+                <!-- This slot will handle the title/header of the accordion and is the part you click on -->
+                <template slot="accordion-trigger">
+                    <h3>Item 2</h3>
+                </template>
+                <!-- This slot will handle all the content that is passed to the accordion -->
+                <template slot="accordion-content">
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+                </template>
+            </topics>
+
+            <topics>
+                <!-- This slot will handle the title/header of the accordion and is the part you click on -->
+                <template slot="accordion-trigger">
+                    <h3>Item 3</h3>
+                </template>
+                <!-- This slot will handle all the content that is passed to the accordion -->
+                <template slot="accordion-content">
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+                </template>
+            </topics>
+        </accordion>
+        </div>
+    </div>
 </template>
 
 <script>
 import NavigationBar from '../components/NavigationBar'
-import SearchBar from '../components/SearchBar'
+import SearchBar from '../components/SearchBar.vue'
+import Accordion from '../components/Accordion.vue'
+import Topics from '../components/Topics.vue'
 
 export default {
   name: 'FAQ',
   components: {
     NavigationBar,
-    SearchBar
+    SearchBar,
     Accordion,
     Topics
   }
+}
 
 </script>
 <style>
@@ -27,14 +66,13 @@ export default {
     margin-top: 100px;
 }
 
-
-<style>
 #faq{
     display: grid;
-    grid-template-columns: 10% auto;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+    grid-template-columns: 30% auto;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+}
 </style>
