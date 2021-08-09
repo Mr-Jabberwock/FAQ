de90ec31a0e28b872063e8074f831de49a671201
Mr-Jabberwock <djohnsen143@gmail.com>
2021-08-09 16:16:54 +0200

    search page. No functionality

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
-import FAQ from './views/FAQ.vue'
-//import App from './App.vue'
+import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(FAQ),
+  render: h => h(App),
 }).$mount('#app')
 
---
+++ src/views/SearchResults.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <search-bar v-on:searchCriteria="searchData"></search-bar>
+        <div v-for="article in filteredArticles" :key="article.id">
+            <div class="list-item">
+                {{article.title}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import SearchBar from "../components/SearchBar.vue"
+import json from "../assets/articles.json"
+
+export default{
+    name: "SearchResults",
+    components:{
+        SearchBar
+    },
+    data() {
+       return {
+           articles: json.articles,
+           searchResult: this.$route.params.criteria
+       }
+    },
+    methods:{
+    },
+    computed:{
+
+    },
+    watch: {
+        $router() {
+            this.searchResult = this.$route.params.id;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.list-item{
+    border-bottom: 2px solid black;
+    background-color: lightgrey;
+    width: 50%;
+}
+</style>
