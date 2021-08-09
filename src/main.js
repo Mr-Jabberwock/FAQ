30c629ffa35e923dfa5c6a5607ff3d712baf8717
Mr-Jabberwock <djohnsen143@gmail.com>
2021-08-09 09:33:31 +0200

    Fixed the router issues

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import FAQ from './views/FAQ.vue'
-//import App from './App.vue'
-import Routes from './router'
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  router: Routes,
-  mode: 'history'
-})
+//import FAQ from './views/FAQ.vue'
+import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router: router,
-  render: h => h(FAQ),
+  router,
+  render: h => h(App),
 }).$mount('#app')
